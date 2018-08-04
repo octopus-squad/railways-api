d583d9db8f1d7267f8c750814ac1f5535e818cb1
karde <kardelen.karatas@gmail.com>
2018-08-04 20:23:35 +0200

    get_stations and add_station

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
+import re
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+ 
+
 
 app = Flask(__name__)
-
+db = MongoClient('localhost').get_database('octopus')
 
 @app.route('/', methods=['GET'])
 def get_home():
@@ -9,3 +13,19 @@ def get_home():
         'app_name': 'railways-api',
         'version': '0.1'
     })
+
+@app.route('/stations', methods=['GET'])
+def get_stations():
+    stations = list(db.stations.find())
+    return jsonify(stations)
+
+@app.route('/stations', methods=['POST'])
+def add_station():
+    json_data = request.get_json()
+    station_id = "".join(re.findall(r'[a-zA-Z]', json_data["name"])).lower()
+    station_name = json_data["name"]
+    station_city = json_data["city"]       
+    db.stations.insert({"_id":station_id, "name":station_name, "city":station_city})
+    
+    return jsonify({"id":station_id})     
+ 
