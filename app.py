451c0bf621dc9ff49f4f4fbc4e5f8254c5a9890f
karde <kardelen.karatas@gmail.com>
2018-08-05 12:36:11 +0200

    get / delete / update station

--- app.py
+++ app.py
@@ -19,13 +19,50 @@ def get_stations():
     stations = list(db.stations.find())
     return jsonify(stations)
 
+
+@app.route('/station/<id>', methods=['GET'])
+def get_station(id):
+    stations = list(db.stations.find())
+    for station in stations:
+        if id == station["_id"]:
+            return_station = {
+                "name" : station["name"],
+                "city" : station["city"],
+                "id" : station["_id"]
+            }
+    
+    return jsonify(return_station)
+
+
 @app.route('/stations', methods=['POST'])
 def add_station():
     json_data = request.get_json()
+
     station_id = "".join(re.findall(r'[a-zA-Z]', json_data["name"])).lower()
     station_name = json_data["name"]
     station_city = json_data["city"]       
     db.stations.insert({"_id":station_id, "name":station_name, "city":station_city})
     
     return jsonify({"id":station_id})     
- 
+
+
+@app.route('/stations/<id>', methods=['DELETE'])
+def delete_station(id):
+    db.stations.remove({"_id": id})    
+    return jsonify({"id":id})
+            
+
+@app.route('/station/<id>', methods=['PUT'])
+def update_station(id):
+    json_data = request.get_json()
+    db.stations.update(
+        {"_id":id},
+        {"$set":
+        {
+            "name":json_data["name"],
+            "city" :json_data["city"]
+            }
+            }
+        )
+    
+    return jsonify({"id":id})
