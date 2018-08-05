759104e211afbcdc80b5256d4beb349c0bc64a1d
karde <kardelen.karatas@gmail.com>
2018-08-05 15:17:48 +0200

    add, update, delete for stations and get, add for lines

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import re
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
- 
+from pymongo.errors import DuplicateKeyError
 
 
 app = Flask(__name__)
 db = MongoClient('localhost').get_database('octopus')
+
 
 @app.route('/', methods=['GET'])
 def get_home():
@@ -14,55 +15,97 @@ def get_home():
         'version': '0.1'
     })
 
+
 @app.route('/stations', methods=['GET'])
 def get_stations():
     stations = list(db.stations.find())
     return jsonify(stations)
 
 
-@app.route('/station/<id>', methods=['GET'])
+@app.route('/stations/<id>', methods=['GET'])
 def get_station(id):
-    stations = list(db.stations.find())
-    for station in stations:
-        if id == station["_id"]:
-            return_station = {
-                "name" : station["name"],
-                "city" : station["city"],
-                "id" : station["_id"]
-            }
-    
-    return jsonify(return_station)
+    station = list(db.stations.find_one({"_id":id}))
+    if station is not None:
+        return jsonify(station)
+    else:
+        error_response = jsonify({"error": "{} doesn't exist in database".format(id)})
+        error_response.status_code = 400
+        return error_response 
 
 
 @app.route('/stations', methods=['POST'])
 def add_station():
     json_data = request.get_json()
-
-    station_id = "".join(re.findall(r'[a-zA-Z]', json_data["name"])).lower()
-    station_name = json_data["name"]
-    station_city = json_data["city"]       
-    db.stations.insert({"_id":station_id, "name":station_name, "city":station_city})
-    
-    return jsonify({"id":station_id})     
+    try:
+        station_id = "".join(re.findall(r'[a-zA-Z]', json_data["name"])).lower()
+        station_name = json_data["name"]
+        station_city = json_data["city"]
+        db.stations.insert(
+            {"_id": station_id, "name": station_name, "city": station_city})
+        return jsonify({"id": station_id})
+    except KeyError as e:
+        error_response = jsonify({"error": "missing data: {}".format(e)})
+        error_response.status_code = 400
+        return error_response
+    except DuplicateKeyError:
+        error_response = jsonify({"error": "station already in database"})
+        error_response.status_code = 400
+        return error_response
 
 
 @app.route('/stations/<id>', methods=['DELETE'])
 def delete_station(id):
-    db.stations.remove({"_id": id})    
-    return jsonify({"id":id})
-            
+    db.stations.remove({"_id": id})
+    return jsonify({"id": id})
 
-@app.route('/station/<id>', methods=['PUT'])
+
+@app.route('/stations/<id>', methods=['PUT'])
 def update_station(id):
     json_data = request.get_json()
     db.stations.update(
-        {"_id":id},
+        {"_id": id},
         {"$set":
-        {
-            "name":json_data["name"],
-            "city" :json_data["city"]
-            }
-            }
-        )
+         {
+             "name": json_data["name"],
+             "city": json_data["city"]
+         }
+         }
+    )
+    return jsonify({"id": id})
+
+
+@app.route('/lines', methods=['GET'])
+def get_lines():
+    lines = list(db.lines.find())
+    return jsonify(lines)
+
+@app.route('/lines/<id>', methods=['GET'])
+def get_line(id):
+        line = db.lines.find_one({"_id":id})
+        if line is not None:
+            return jsonify(line)
+        else:
+            error_response = jsonify({"error": "{} doesn't exist in database".format(id)})
+            error_response.status_code = 400
+            return error_response
+
+
+
+@app.route('/lines', methods=['POST'])
+def add_line():
+    try:
+        json_data = request.get_json()
+        line_id = "".join(re.findall(r'[a-zA-Z]', json_data["name"])).lower()
+        line_name = json_data["name"]
+        line_stops = json_data["stops"]
+        db.lines.insert({"_id":line_id, "name":line_name, "stops":line_stops})
+        return jsonify({"id": line_id})
+    except KeyError as e:
+        error_response = jsonify({"error": "missing data: {}".format(e)})
+        error_response.status_code = 400
+        return error_response
+    except DuplicateKeyError:
+        error_response = jsonify({"error": "line already in database"})
+        error_response.status_code = 400
+        return error_response
     
-    return jsonify({"id":id})
